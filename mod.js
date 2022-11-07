2ccc77f8de8527fdbc65e9c7b04b012ef9a99318
Michael McGranaghan <M.McGranaghan@opustech.co.uk>
2022-11-07 06:32:08 +0000

    clean up of mod.js

--- mod.js
+++ mod.js
@@ -1,22 +1,7 @@
 
 import { MetricModule, FirstEncounter } from "./lib/metricmodules.js";
 
-/** Returns `Hello World` in bold */
-export function multiplyNumbers(a, b) {
-  return a * b;
-}
-
-/**Says hello to General Kenobi */
-export function HelloThere() {
-    alert("... General Kenobi");
-}
-
-export function Greeting(name) {
-    alert(`Hi, ${name}`);
-}
-
 export {
     MetricModule,
     FirstEncounter
 }
-
